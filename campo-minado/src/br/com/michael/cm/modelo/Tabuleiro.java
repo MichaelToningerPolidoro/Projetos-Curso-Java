63ed5f84b4b5bfa893dd6f2cf9d96538a9556e33
Michael Toninger <michaeltp.2012@hotmail.com>
2021-02-19 14:13:36 -0300

    Concluido funcionalidades classe Tabuleiro

--- campo-minado/src/br/com/michael/cm/modelo/Tabuleiro.java
+++ campo-minado/src/br/com/michael/cm/modelo/Tabuleiro.java
@@ -3,19 +3,20 @@ package br.com.michael.cm.modelo;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Tabuleiro {
 
 	private int linhas;
 	private int colunas;
-	private int minas;
+	private int minasExigidas;
 	
 	private final List<Campo> campos = new ArrayList<>();
 
 	public Tabuleiro(int linhas, int colunas, int minas) {
 		this.linhas = linhas;
 		this.colunas = colunas;
-		this.minas = minas;
+		this.minasExigidas = minas;
 		
 		gerarCampos();
 		associarVizinhos();
@@ -39,6 +40,27 @@ public class Tabuleiro {
 	}
 	
 	private void sortearMinas() {
+		long minasArmadas = 0;
+		Predicate<Campo> minado = campo -> campo.isMinado();
 		
+		do {
+			minasArmadas = campos.stream().filter(minado).count();
+			int aleatorio = (int) (Math.random() * campos.size());
+			campos.get(aleatorio).minar();
+			
+		} while (minasArmadas < minasExigidas);
+	}
+	
+	public boolean objetivoAlcancado() {
+		return campos.stream().allMatch(c -> c.objetivoAlcancado());
+	}
+	
+	public void reiniciarJogo() {
+		campos.parallelStream().forEach(c -> c.reinicar());
+		sortearMinas();
+	}
+	
+	public String toString() {
+		return "";
 	}
 }
