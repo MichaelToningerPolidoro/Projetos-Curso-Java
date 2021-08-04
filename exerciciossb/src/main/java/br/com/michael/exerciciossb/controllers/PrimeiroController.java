5f562dd1479471283bb9117132cea7cc727a47de
Michael Toninger <michaeltp.2012@hotmail.com>
2021-08-04 17:47:47 -0300

    Adicionado primeiro metodo post provisorio

--- exerciciossb/src/main/java/br/com/michael/exerciciossb/controllers/PrimeiroController.java
+++ exerciciossb/src/main/java/br/com/michael/exerciciossb/controllers/PrimeiroController.java
@@ -1,6 +1,7 @@
 package br.com.michael.exerciciossb.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,10 @@ public class PrimeiroController {
 	@GetMapping(path = {"/olamundo", "/saudacao"})
 	public String hello() {
 		return "Hello World!! Vou aprender Spring Boot!!";
+	}
+	
+	@PostMapping(path = "/saudacao-post")
+	public String helloPost() {
+		return "Hello world POST";
 	}
 }
