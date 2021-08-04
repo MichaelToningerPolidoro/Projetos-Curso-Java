ba54980b537b5e02431188e92c70b814a8df0f8a
Michael Toninger <michaeltp.2012@hotmail.com>
2021-08-04 17:34:45 -0300

    Primeiro commit projeto spring bootk

---
+++ exerciciossb/src/test/java/br/com/michael/exerciciossb/ExerciciossbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.michael.exerciciossb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciciossbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
