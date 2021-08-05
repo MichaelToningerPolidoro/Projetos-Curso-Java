2c7feada8cfc2d99afd039af4194bcdbc76864f6
Michael Toninger <michaeltp.2012@hotmail.com>
2021-08-05 16:56:51 -0300

    Passando valores para as requisicoes

--- exerciciossb/src/main/java/br/com/michael/exerciciossb/controllers/ClienteController.java
+++ exerciciossb/src/main/java/br/com/michael/exerciciossb/controllers/ClienteController.java
@@ -1,6 +1,7 @@
 package br.com.michael.exerciciossb.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,13 @@ import br.com.michael.exerciciossb.models.Cliente;
 public class ClienteController {
 
 	@GetMapping("/qualquer")
-	public Cliente obterCliente() {
+	public static Cliente obterCliente() {
 		return new Cliente(1, "José", "12345678910");
 	}
+	
+	@GetMapping("/{id}")
+	public static Cliente obterClientePorId1(@PathVariable int id) {
+		return new Cliente(id, "Maria", "12345678910");
+	}
+	
 }
