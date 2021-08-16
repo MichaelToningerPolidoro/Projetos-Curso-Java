5fd05096b0b8737a4c37d8e5f8cbdb9f1757cc96
Michael Toninger <michaeltp.2012@hotmail.com>
2021-08-16 17:20:55 -0300

    Reaproveitando metodo para put e post

--- exerciciossb/src/main/java/br/com/michael/exerciciossb/controllers/ProdutoController.java
+++ exerciciossb/src/main/java/br/com/michael/exerciciossb/controllers/ProdutoController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +24,7 @@ public class ProdutoController {
 	@Autowired
 	private ProdutoRepository produtoRepository;
 	
-	@PostMapping
+	@RequestMapping(method = {RequestMethod.POST, RequestMethod.PUT})
 	public @ResponseBody Produto novoProduto(@Valid Produto produto) {
 		produtoRepository.save(produto);
 		return produto;
@@ -37,10 +38,5 @@ public class ProdutoController {
 	@GetMapping(path="/{id}")
 	public Optional<Produto> obterProdutoPorId(@PathVariable int id) {
 		return produtoRepository.findById(id);
-	}
-	
-	@PutMapping
-	public Produto atualizarProduto(@Valid Produto produto) {
-		return produtoRepository.save(produto);
 	}
 }
