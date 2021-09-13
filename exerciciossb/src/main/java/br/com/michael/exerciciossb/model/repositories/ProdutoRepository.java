941f564eabe7b9a7b8d7b362d183f6a4630ca2e3
Michael Toninger <michaeltp.2012@hotmail.com>
2021-09-13 17:18:14 -0300

    Finalizado fundamentos de Spring boot

--- exerciciossb/src/main/java/br/com/michael/exerciciossb/model/repositories/ProdutoRepository.java
+++ exerciciossb/src/main/java/br/com/michael/exerciciossb/model/repositories/ProdutoRepository.java
@@ -1,10 +1,24 @@
 package br.com.michael.exerciciossb.model.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 import br.com.michael.exerciciossb.model.entities.Produto;
 
 public interface ProdutoRepository extends PagingAndSortingRepository<Produto, Integer> {
 
 	public Iterable<Produto> findByNomeContainingIgnoreCase(String parteNome);
+	
+	// mais algumas convenções
+	// findByNomeContaining
+	// findByNomeContains
+	// findByNomeIsContaining
+	// findByNomeNotContaining
+	
+	// findByNomeStartsWith
+	// findByNomeEndsWith
+	
+	@Query("SELECT p FROM Produto p where p.nome LIKE %:nome%")
+	public Iterable<Produto> searchByNameLike(@Param("nome") String nome);
 }
