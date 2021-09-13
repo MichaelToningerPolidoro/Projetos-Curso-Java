c759958a1b1df37dd52c2a203792a2831f131c91
Michael Toninger <michaeltp.2012@hotmail.com>
2021-09-13 17:12:40 -0300

    Finalizado consultas paginadas e por nome

--- exerciciossb/src/main/java/br/com/michael/exerciciossb/controllers/ProdutoController.java
+++ exerciciossb/src/main/java/br/com/michael/exerciciossb/controllers/ProdutoController.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,6 +36,22 @@ public class ProdutoController {
 	@GetMapping
 	public Iterable<Produto> obterProdutos() {
 		return produtoRepository.findAll();
+	}
+	
+	@GetMapping(path="/nome/{parteNome}")
+	public Iterable<Produto> obterProdutosPorNome(@PathVariable String parteNome) {
+		return produtoRepository.findByNomeContainingIgnoreCase(parteNome);
+	}
+	
+	@GetMapping(path = "/pagina/{numeroPagina}/{qtdeItems}")
+	public Iterable<Produto> obterProdutosPorPagina(@PathVariable int numeroPagina, @PathVariable int qtdeItems) {
+		
+		if (qtdeItems <= 0 || qtdeItems > 10) {
+			qtdeItems = 10;
+		}
+		
+		Pageable pageable = PageRequest.of(numeroPagina, qtdeItems);
+		return produtoRepository.findAll(pageable);
 	}
 	
 	@GetMapping(path = PATH_ID)
--- exerciciossb/src/main/java/br/com/michael/exerciciossb/model/repositories/ProdutoRepository.java
+++ exerciciossb/src/main/java/br/com/michael/exerciciossb/model/repositories/ProdutoRepository.java
@@ -1,9 +1,10 @@
 package br.com.michael.exerciciossb.model.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import br.com.michael.exerciciossb.model.entities.Produto;
 
-public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
+public interface ProdutoRepository extends PagingAndSortingRepository<Produto, Integer> {
 
+	public Iterable<Produto> findByNomeContainingIgnoreCase(String parteNome);
 }
